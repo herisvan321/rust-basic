e9f335f38396f32125cec3424ca2620d0262d5be
batatu <herisvan321@gmail.com>
2026-05-06 09:12:03 +0700

    style: fix indentation in migration module and web router definition

--- src/routes/web.rs
+++ src/routes/web.rs
@@ -3,7 +3,7 @@ use crate::app::http::controllers::welcome_controller;
 use crate::config::server::AppState;
 
 pub fn router() -> Router<AppState> {
-    Router::new()
+        Router::new()
         .route("/", get(welcome_controller::index))
         .route("/dev", get(welcome_controller::dev_info))
         .route("/test-rsx", get(welcome_controller::test_rsx))
